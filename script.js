62fb4bba6622843bfab7aecd2e256f74740c12a8
Casyburnsed <Casyburnsed@gmail.com>
2024-06-09 20:08:11 -0700

    wrote css and completed js functions

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+const display = document.getElementById('display');
 
 function number(num) {
-
+    display.value +=num;
 }
 
 function calculate() {
-
+    try {
+        display.value = eval(display.value.replace(/x/g, '*').replace(/÷/g, '/'));
+    } catch (e) {
+        display.value = 'Error';
+    }
 }
 
 
 function clearDisplay() {
-
+    display.value = '';
 }
 
 function deleteLast() {
-
+    display.value = display.value.slice(0, -1);
 }
